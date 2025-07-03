6ac399c707ef76464fda6d37177532941fb3acf0
boehm-c <connor.beames@gmail.com>
2025-07-03 12:04:12 -0600

    adding recursive and multiple pages from content directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from markdown_blocks import markdown_to_html_node, extract_title
 from os import listdir, mkdir, makedirs
-from os.path import isfile, join, dirname, exists
+from os.path import isfile, join, dirname, exists, splitext
 import shutil
 
 
@@ -31,6 +31,19 @@ def copy_static_directory(public_dir, static_path):
             mkdir(new_item_path)
             copy_static_directory(new_item_path, item_path)
 
+def copy_content_directory(public_dir, content_path, template_file_path):
+    current_items = listdir(content_path)
+    for dir_item in current_items:
+        item_path = join(content_path, f"{dir_item}")
+        if isfile(item_path):
+            file_name, file_ext = splitext(dir_item)
+            new_item_path = join(public_dir, f"{file_name}.html")
+            generate_page(item_path, template_file_path, new_item_path)
+        else:
+            new_item_path = join(public_dir, f"{dir_item}")
+            mkdir(new_item_path)
+            copy_content_directory(new_item_path, item_path, template_file_path)
+
 
 def create_public_directory(public_dir):
     if exists(public_dir):
@@ -42,14 +55,20 @@ def main():
     current_dir_name = dirname(__file__)
     static_dir = join(current_dir_name, "../static")
     public_dir = join(current_dir_name, "../public")
+    content_dir = join(current_dir_name, "../content")
+    template_file_path = join(current_dir_name, "../template.html")
+
     create_public_directory(public_dir)
     copy_static_directory(public_dir, static_dir)
+    copy_content_directory(public_dir, content_dir, template_file_path)
 
-    content_dir = join(current_dir_name, "../content")
-    index_md_file_path = join(content_dir, "index.md")
-    template_file_path = join(current_dir_name, "../template.html")
-    index_html_file_path = join(public_dir, "index.html")
-    generate_page(index_md_file_path, template_file_path, index_html_file_path)
+
+    # index_md_file_path = join(content_dir, "index.md")
+    # index_html_file_path = join(public_dir, "index.html")
+
+
+
+    # generate_page(index_md_file_path, template_file_path, index_html_file_path)
 
 
 if __name__ == "__main__":
